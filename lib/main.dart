85448d448c6edb6c2fb57ab6fe8d8e1020c905e2
Manuel Aragon <aragon.manuel@live.com>
2022-10-31 20:28:45 -0500

    Manually merged camera screen to app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
 import 'package:lucky13capstone/register.dart';
 import 'package:lucky13capstone/login.dart';
+import 'package:lucky13capstone/camera.dart';
+
+
 
 
 
 void main() async {
+
+
     WidgetsFlutterBinding.ensureInitialized(); 
     await Firebase.initializeApp(); 
+
+  // //camera
+  // // Obtain a list of the available cameras on the device.
+  // final cameras = await availableCameras();
+
+  // // Get a specific camera from the list of available cameras.
+  // final firstCamera = cameras.first;
+
+
+
     runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(title: 'Home Page'),
