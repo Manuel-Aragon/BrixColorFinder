7d58a238846ab9b1103aff101589a66fa52ca80f
Manuel Aragon <aragon.manuel@live.com>
2022-11-02 18:15:37 -0500

    login and registration are now buttons

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lucky13capstone/register.dart';
+import 'package:lucky13capstone/tempScan.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -12,134 +14,134 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      body: Container(
+      appBar: AppBar(title: Text("Login")),
+      body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            Container(
+            Stack(
+              children: <Widget>[
+                Container(
                 height: 370,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/images/lego.png'),
-                        fit: BoxFit.fill
-                    )
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/lego.png'),
+                          fit: BoxFit.fill
+                      )
+                  ),
                 ),
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                        child: Container(
-                            margin: const EdgeInsets.only(top: 300),
-                            child: const Center(
-                              child: Text("Login", style: TextStyle(color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold),),
-                            )
-                        )
-                    )
-                  ],
+                Container(
+                  margin: const EdgeInsets.only(top: 300),
+                  child: const Center(
+                    child: Text("Login", style: TextStyle(color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold),),
+                  )
                 )
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          const BoxShadow(
-                              color: Color.fromRGBO(143, 148, 251, .2),
-                              blurRadius: 20.0,
-                              offset: Offset(0,10)
-                          )
-                        ]
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          padding: const EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                              color: Colors.grey[100],
-                              border: Border(bottom: BorderSide(color: Colors.grey.shade100))
-                          ),
-                          child: TextField(
-                            decoration: InputDecoration(
-                                hintText: " Email or Phone number",
-                                prefixIcon: const Icon(Icons.person, color: Colors.redAccent),
-                                hintStyle: TextStyle(color: Colors.grey[500])
-                            ),
+            Column(
+              children: <Widget>[
+                Container( //login form
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        const BoxShadow(
+                            color: Color.fromRGBO(143, 148, 251, .2),
+                            blurRadius: 20.0,
+                            offset: Offset(0,10)
+                        )
+                      ]
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[100],
+                            border: Border(bottom: BorderSide(color: Colors.grey.shade100))
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              hintText: " Email or Phone number",
+                              prefixIcon: const Icon(Icons.person, color: Colors.redAccent),
+                              hintStyle: TextStyle(color: Colors.grey[500])
                           ),
                         ),
-                        Container(
-                          padding: const EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                              color: Colors.grey[100],
-                              border: Border(bottom: BorderSide(color: Colors.grey.shade100))
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[100],
+                            border: Border(bottom: BorderSide(color: Colors.grey.shade100))
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              hintText: "Password",
+                              prefixIcon: const Icon(Icons.lock_outline, color: Colors.redAccent),
+                              hintStyle: TextStyle(color: Colors.grey[500])
                           ),
-                          child: TextField(
-                            decoration: InputDecoration(
-                                hintText: "Password",
-                                prefixIcon: const Icon(Icons.lock_outline, color: Colors.redAccent),
-                                hintStyle: TextStyle(color: Colors.grey[500])
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
+                        ),
+                      )
+                    ],
                   ),
-                  const SizedBox(height: 30,),
-                  Container(
-                    height: 50,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: const LinearGradient(
-                            colors: [
-                              Color.fromRGBO(16, 20, 251, 1),
-                              Color.fromRGBO(16, 20, 251, .6),
-                            ]
-                        )
-                    ),
+                ),
+                const SizedBox(height: 10),
+                Container(  //login button
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: const LinearGradient(
+                        colors: [
+                          Color.fromRGBO(16, 20, 251, 1),
+                          Color.fromRGBO(16, 20, 251, .6),
+                        ]
+                    )
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {
+                        Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ScanPage()),
+                        );
+                      },
+                    style: ElevatedButton.styleFrom(primary: Colors.transparent, shadowColor: Colors.transparent),
                     child: const Center(
                       child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
                     ),
                   ),
-                  const SizedBox(height: 30,),
-                  Container(
-                    height: 50,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: const LinearGradient(
-                            colors: [
-                              Color.fromRGBO(16, 20, 251, 1),
-                              Color.fromRGBO(16, 20, 251, .6),
-                            ]
-                        )
-                    ),
+                ),
+                const SizedBox(height: 10),
+                Container(  //registration button
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: const LinearGradient(
+                        colors: [
+                          Color.fromRGBO(16, 20, 251, 1),
+                          Color.fromRGBO(16, 20, 251, .6),
+                        ]
+                    )
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {
+                        Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUpPage()),
+                        );
+                      },
+                    style: ElevatedButton.styleFrom(primary: Colors.transparent, shadowColor: Colors.transparent),
                     child: const Center(
                       child: Text("Register", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
                     ),
-                  )
-                ],
-              ),
-            )
+                  ),
+                ),
+                const SizedBox(height: 10),
+              ],
+            ),
           ],
         ),
       ),
-      // Remove this after testing is over
-      bottomNavigationBar: BottomAppBar(  //nav bar to hold return button
-        shape: const CircularNotchedRectangle(),
-        child: Container(
-          height: 50.0,
-          child : ElevatedButton( //return button
-            child: const Text('Go back to home screen'),
-            onPressed: () {
-                Navigator.pop(context);
-            },
-          ),  //elevated button
-          ),//container
-      ),//container
-      //Remove this section after testing is over
     );
   }
 }
