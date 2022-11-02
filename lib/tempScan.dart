0cea8a8337124bfb1906a0a8315ee4af1e07b3bc
Manuel Aragon <aragon.manuel@live.com>
2022-11-02 18:43:53 -0500

    added buttons to scan page

--- lib/tempScan.dart
+++ lib/tempScan.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:lucky13capstone/brickview.dart';
+import 'settings.dart';
 class ScanPage extends StatefulWidget {
   const ScanPage({Key? key}) : super(key: key);
 
@@ -15,6 +16,32 @@ class _ScanPageState extends State<ScanPage> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text("Scan"),
+      ),
+     body:  Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+              child: Column(
+                children: <Widget>[
+                  ElevatedButton( //settings
+                    child: const Text('Settings'),
+                      onPressed: () {
+                         Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SettingsPage()),
+                          );
+                        },
+                  ),
+                  ElevatedButton( //brick view
+                    child: const Text('Brick View'),
+                      onPressed: () {
+                         Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => BrickView()),
+                          );
+                        },
+                  ),
+                ],
+              ),
       ),
     );
   }
