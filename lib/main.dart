1fcb030fed7e4e11706068a4944a88250247fb65
Manuel Aragon <aragon.manuel@live.com>
2022-11-15 09:27:38 -0600

    removed comments

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,7 @@ import 'package:lucky13capstone/home.dart';
 void main() async {
 
     WidgetsFlutterBinding.ensureInitialized(); 
-    await Firebase.initializeApp(); 
-
-  // //camera
-  // // Obtain a list of the available cameras on the device.
-  //final cameras = await availableCameras();
-
-  // // Get a specific camera from the list of available cameras.
-  //final firstCamera = cameras.first;
+    await Firebase.initializeApp(); //initialize firebase
 
     runApp(const MaterialApp(
       debugShowCheckedModeBanner: false,
