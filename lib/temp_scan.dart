4f9e24b16d260ce4b35be5df2dd5112437f1d203
phamshellie <105142377+phamshellie@users.noreply.github.com>
2022-12-06 04:47:24 -0600

    Update temp_scan.dart
    scan page update

--- lib/temp_scan.dart
+++ lib/temp_scan.dart
@@ -19,30 +19,33 @@ class _ScanPageState extends State<ScanPage> {
       appBar: AppBar(
         title: const Text("Scan"),
       ),
-     body:  Center(
+    /* body:  Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
               child: Column(
                 children: <Widget>[
-                  ElevatedButton( //settings
-                    child: const Text('Settings'),
+                  IconButton(
+                  //ElevatedButton( //settings
+                    //child: const Text('Setting'),
                       onPressed: () {
                          Navigator.push(
                           context,
                           MaterialPageRoute(builder: (context) => const SettingsPage()),
                           );
                         },
+                        icon: Icon(Icons.settings),
                   ),
-                  ElevatedButton( //brick view
-                    child: const Text('History'),
+                  IconButton( //brick view
+                    //child: const Text('History'),
                       onPressed: () {
                          Navigator.push(
                           context,
                           MaterialPageRoute(builder: (context) => const HistoryPage()),
                           );
                         },
+                        icon: Icon(Icons.access_time),
                   ),
-                  ElevatedButton( //brick view
+                 /* ElevatedButton( //brick view
                     child: const Text('Brick View'),
                       onPressed: () {
                          Navigator.push(
@@ -50,10 +53,139 @@ class _ScanPageState extends State<ScanPage> {
                           MaterialPageRoute(builder: (context) => const BrickView()),
                           );
                         },
-                  ),
+                  ),*/
+                  ElevatedButton.icon(
+                    onPressed: (){},
+                    icon: Icon(Icons.camera_alt_outlined),
+                    label: Text('SCAN'),
+                    ), 
                 ],
               ),
-      ),
+      ),*/
+
+      /*body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+                   onPressed: () {
+                      Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const SettingsPage()),
+                      );
+                    },
+                    icon: Icon(Icons.settings),
+                  ),
+                  IconButton( 
+                      onPressed: () {
+                         Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const HistoryPage()),
+                          );
+                        },
+                        icon: Icon(Icons.access_time),
+                  ),
+        ],
+      ),*/
+     
+
+     /* Align(
+        alignment: Alignment.bottomCenter,
+        child: ElevatedButton.icon(
+                    onPressed: (){},
+                    icon: Icon(Icons.camera_alt_outlined),
+                    label: Text('SCAN'),
+                    ), 
+                    child:widget
+            ),
+
+            */
+
+
+    body: Column(
+      children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+            IconButton(
+                   onPressed: () {
+                      Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const SettingsPage()),
+                      );
+                    },
+                    icon: Icon(Icons.settings),
+                  ),
+            IconButton( 
+                      onPressed: () {
+                         Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const HistoryPage()),
+                          );
+                        },
+                        icon: Icon(Icons.access_time),
+                  ),
+              ],
+            ),
+        SizedBox(height: 450), 
+
+         Column(
+          //mainAxisAlignment: MainAxisAlignment.spaceAround,
+           children: [
+           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text('Shape      '),
+              SizedBox(
+                width: 150,
+                height: 40,
+                child: const DecoratedBox(
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 221, 221, 221)),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height:10),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text('Dimension'),
+              SizedBox(
+                width: 150,
+                height: 40,
+                child: const DecoratedBox(
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 221, 221, 221)),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height:10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text('Color        '),
+              SizedBox(
+                width: 150,
+                height: 40,
+                child: const DecoratedBox(
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 221, 221, 221)),
+                ),
+              ),
+            ],
+          ),
+          ],
+        ),
+       SizedBox(height:20),
+        ElevatedButton.icon(
+                    onPressed: (){},
+                    icon: Icon(Icons.camera_alt_outlined),
+                    label: Text('SCAN'),
+                    ), 
+      ],
+    ),
+
     );
   }
 }
