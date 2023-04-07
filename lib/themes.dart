47cd025e4c9a26f0008050620a59bc0b2911b9e7
Miguelh346 <miguelh7194@gmail.com>
2023-04-07 04:04:32 -0500

    Dark mode color changes

--- lib/themes.dart
+++ lib/themes.dart
@@ -17,17 +17,20 @@ final ThemeData lightTheme = ThemeData(
 );
 
 final ThemeData darkTheme = ThemeData(
-  scaffoldBackgroundColor: const Color.fromARGB(255, 25, 39, 48),
-  appBarTheme: const AppBarTheme(color: Color.fromARGB(223, 212, 89, 100)),
+  scaffoldBackgroundColor: const Color(0xFF000000),
+  appBarTheme: const AppBarTheme(color: Color.fromARGB(223, 67, 64, 65)),
   listTileTheme: const ListTileThemeData(
-    textColor: Color.fromARGB(255, 38, 214, 226),
-    iconColor: Color.fromARGB(255, 38, 214, 226),
+    textColor: Color.fromARGB(255, 255, 255, 255),
+    iconColor: Color.fromARGB(255, 255, 255, 255),
   ),
   elevatedButtonTheme: ElevatedButtonThemeData(
     style: ElevatedButton.styleFrom(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      backgroundColor: const Color.fromARGB(255, 38, 214, 226),
-      foregroundColor: const Color.fromARGB(255, 25, 39, 48),
+      backgroundColor: const Color.fromARGB(255, 67, 64, 65),
+      foregroundColor: const Color.fromARGB(255, 255, 255, 255),
     ),
+  ),
+  dividerTheme: const DividerThemeData(
+    color: Color.fromARGB(255, 69, 68, 68),
   ),
 );
